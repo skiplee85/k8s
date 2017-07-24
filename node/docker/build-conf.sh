538a09c55636e70372b6ac77f119da67dca7a4b9
lishijun <lishijun@sensefun.com>
2017-07-24 17:03:40 +0800

    change mode to LF

--- node/docker/build-conf.sh
+++ node/docker/build-conf.sh
@@ -3,25 +3,25 @@ flannel_env="/etc/kubernetes/flannel/subnet.env"
 docker_conf="/etc/kubernetes/docker/daemon.json"
 
 if [ -f "$flannel_env" ]; then
-	. $flannel_env
+  . $flannel_env
 fi
 
 echo "{\"registry-mirrors\":[\"https:\/\/7kwfkwia.mirror.aliyuncs.com\"]" > $docker_conf
 
 if [ -n "$FLANNEL_SUBNET" ]; then
-	echo ",\"bip\":\"$FLANNEL_SUBNET\"" >> $docker_conf
+  echo ",\"bip\":\"$FLANNEL_SUBNET\"" >> $docker_conf
 fi
 
 if [ -n "$FLANNEL_MTU" ]; then
-	echo ",\"mtu\":$FLANNEL_MTU" >> $docker_conf
+  echo ",\"mtu\":$FLANNEL_MTU" >> $docker_conf
 fi
 
 if [ -n "$FLANNEL_IPMASQ" ] && [ $ipmasq = true ] ; then
-	if [ "$FLANNEL_IPMASQ" = true ] ; then
-		echo ",\"ip-masq\":false" >> $docker_conf
-	elif [ "$FLANNEL_IPMASQ" = false ] ; then
-		echo ",\"ip-masq\"true" >> $docker_conf
-	fi
+  if [ "$FLANNEL_IPMASQ" = true ] ; then
+    echo ",\"ip-masq\":false" >> $docker_conf
+  elif [ "$FLANNEL_IPMASQ" = false ] ; then
+    echo ",\"ip-masq\"true" >> $docker_conf
+  fi
 fi
 
 echo "}" >> $docker_conf
